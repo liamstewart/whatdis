c638a3ee1be3de61a15ef19292386653b2faa2a8
Liam Stewart <liam@squozen.org>
2024-11-04 06:45:07 -0600

    A bit more request logging

--- go.mod
+++ go.mod
@@ -6,6 +6,7 @@ require (
 	github.com/BurntSushi/toml v1.4.0 // indirect
 	github.com/beorn7/perks v1.0.1 // indirect
 	github.com/cespare/xxhash/v2 v2.3.0 // indirect
+	github.com/felixge/httpsnoop v1.0.4 // indirect
 	github.com/klauspost/compress v1.17.9 // indirect
 	github.com/munnerz/goautoneg v0.0.0-20191010083416-a7dc8b61c822 // indirect
 	github.com/prometheus/client_golang v1.20.5 // indirect
---
+++ internal/middleware.go
@@ -0,0 +1,43 @@
+package internal
+
+import (
+	"net/http"
+
+	"github.com/felixge/httpsnoop"
+	"go.uber.org/zap"
+)
+
+func RecoveryHandler(h http.Handler) http.Handler {
+	m := func(w http.ResponseWriter, req *http.Request) {
+		defer func() {
+			if err := recover(); err != nil {
+				w.WriteHeader(http.StatusInternalServerError)
+				// TODO: log
+			}
+		}()
+
+		h.ServeHTTP(w, req)
+	}
+
+	return http.HandlerFunc(m)
+}
+
+func LoggingHandler(h http.Handler, logger *zap.SugaredLogger) http.Handler {
+	wrapped := func(w http.ResponseWriter, req *http.Request) {
+		m := httpsnoop.CaptureMetrics(h, w, req)
+		// TODO: force omit caller, other fields?
+		logger.Infow(
+			"request",
+			"request_url", req.URL,
+			"protocol", req.Proto,
+			"request_method", req.Method,
+			"status_code", m.Code,
+			"response_size_bytes", m.Written,
+			"response_time_secs", m.Duration,
+			"user_id", "-",
+			"user_name", "-",
+		)
+	}
+
+	return http.HandlerFunc(wrapped)
+}
